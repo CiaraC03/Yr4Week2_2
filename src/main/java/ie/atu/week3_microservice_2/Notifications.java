6ddd9e841e9c855c4883abd63dd350800a6ee1e7
G00402357@atu.ie <g00402357@atu.ie>
2024-09-30 17:42:47 +0100

    updates

--- src/main/java/ie/atu/week3_microservice_2/Notifications.java
+++ src/main/java/ie/atu/week3_microservice_2/Notifications.java
@@ -1,4 +1,26 @@
 package ie.atu.week3_microservice_2;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notifications {
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Positive
+    private int age;
+
 }
