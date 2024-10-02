67e59efe0059e1f3bbb912715cbe798b2f9ed808
G00402357@atu.ie <g00402357@atu.ie>
2024-10-02 19:22:18 +0100

    Created customer class and a confirm Notifcations function which confirms if customer has been created. Also assigned to port 8081

---
+++ src/main/java/ie/atu/week3_microservice_2/Customer.java
@@ -0,0 +1,25 @@
+package ie.atu.week3_microservice_2;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Positive
+    private int age;
+
+
+}
